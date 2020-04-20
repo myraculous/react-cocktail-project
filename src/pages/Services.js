4fb1b897faae8de6e0f0491463c374d4276cbbc7
Miraculous-V <dusworld@gmail.com>
2020-04-20 12:31:57 +0100

    services page update

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const Services = () => {
     return (
-        <div>
-            Welcome to Services page
-        </div>
+        <section className = "section about-section">
+      <h1 className="section-title">our services</h1>
+      <p>I'm baby actually normcore portland taxidermy you probably haven't heard of them schlitz, brunch franzen selfies +1 offal anim intelligentsia. Stumptown humblebrag shabby chic nisi, aesthetic gluten-free ullamco slow-carb. Jean shorts readymade hot chicken, sartorial cardigan aliquip four loko post-ironic subway tile church-key</p>
+    </section>
     )
 }
 
