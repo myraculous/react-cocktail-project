686db86382a02eb25326fab2c81de5e831d3ada4
Miraculous-V <dusworld@gmail.com>
2020-04-20 12:52:57 +0100

    linked logo to home page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,9 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="nav-center">
+      <Link to="/">
       <img src={logo} alt="cocktail logo" className="logo" />
+      </Link>
       <ul className="nav-links">
         <li>
           <Link to="/">Home</Link>
